4fa20f38a54e8d46bc211b62fc5004231464b63a
nunzio giulio caggegi <nunziogiuliocaggegi@gmail.com>
2019-06-05 20:00:44 +0200

    Initial Commit

---
+++ testSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  testSwiftUI
+//
+//  Created by nunzio giulio caggegi on 05/06/2019.
+//  Copyright © 2019 nunzio giulio caggegi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
